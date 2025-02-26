90f048e6a3daf905faa3a7fbb1ef19d8f9549a1b
ErlMat <erlend.matre@hotmail.com>
2025-02-26 14:31:02 +0100

    En liten endring

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 `Real Time Temperature: ${previoustemp}°CA` : "No data available";
 
         //
-        database.ref('temperatures').limitToLast(30).once('value').then(function(snapshot) {
+        db.ref('temperatures').limitToLast(30).once('value').then(function(snapshot) {
             const data = snapshot.val();  // Hent data fra Firebase
             const xValues = [];  // Tidspunkt (eller indekser)
             const yValues = [];  // Temperaturdata
