b430fd24094c1d49ab48b31e65a7d430a116689f
JacobFEide <jacobfeide99@gmail.com>
2025-02-26 11:09:57 +0100

    fordeling av js og css

---
+++ public/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    try {
+        // Initialize Firebase
+        const app = firebase.app();
+        const db = firebase.database();
+
+        // Reference to the 'previousTemperature' under 'sensor'
+        const dataRef = db.ref("sensor/previoustemp");
+
+        // Fetch and display data
+        dataRef.on("value", (snapshot) => {
+            const previoustemp = snapshot.val();
+            document.getElementById("dataContainer").textContent = previoustemp !== null ? 
+                `Real Time Temperature: ${previoustemp}°C` : "No data available";
+        });
+
+    } catch (error) {
+        console.error("Firebase initialization error:", error);
+        document.getElementById("dataContainer").textContent = "Error loading Firebase";
+    }
+});
